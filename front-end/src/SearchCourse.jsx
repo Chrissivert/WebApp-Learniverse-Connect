f0a321264587453990f5b8965a56687e7dbf5c78
chris <chrisssy@stud.ntnu.no>
2024-02-12 14:06:38 +0100

    added simple searchbar

--- front-end/src/SearchCourse.jsx
+++ front-end/src/SearchCourse.jsx
@@ -1,10 +1,26 @@
-// SearchCoursesPage.js
-import React from 'react';
+import React, { useState } from 'react';
+import NavigationBar from './components/navbar/Navbar';
 
 function SearchCoursesPage() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div>
+      <NavigationBar/>
       <h1>Search Courses Page</h1>
+      <input
+        type="text"
+        placeholder="Search for courses"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <button onClick={() => alert(`Searching for: ${searchTerm}`)}>
+        Search
+      </button>
     </div>
   );
 }
