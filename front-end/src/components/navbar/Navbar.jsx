50a9adace92cb6f9bb73fa27dcf1b51de17dc08e
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-02-14 17:34:56 +0100

    Commented out all objects, such as 'login', 'cart', 'search', etc.

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -14,26 +14,28 @@ const NavigationBar = () => {
   };
 
   return (
-    <Navbar expand="lg" className="nav">
-      <Container fluid>
-        <Navbar.Brand className="mr-auto">
+    <header>
+      <Navbar expand="lg" className="nav">
+        <Container fluid>
+          <Navbar.Brand className="mr-auto">
+          </Navbar.Brand>
+          {/* <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="search">Search</Nav.Link>
+            <Nav.Link href="about">About Us</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+          </Nav> */}
           <Logo />
-        </Navbar.Brand>
-        <Nav className="me-auto middle-nav">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="search">Search</Nav.Link>
-          <Nav.Link href="about">About Us</Nav.Link>
-          <Nav.Link href="contact">Contact</Nav.Link>
-        </Nav>
-        <Nav className="ms-auto">
-          <Nav.Link href="login">Login</Nav.Link>
-          <Nav.Link href="cart">
-            <CartIcon colorMode={colorMode} />
-          </Nav.Link>
-          <button onClick={toggleColorMode}>Toggle Color Mode</button>
-        </Nav>
-      </Container>
-    </Navbar>
+          {/* <Nav className="ms-auto">
+            <Nav.Link href="login">Login</Nav.Link>
+            <Nav.Link href="cart">
+              <CartIcon colorMode={colorMode} />
+            </Nav.Link>
+            <button onClick={toggleColorMode}>Toggle Color Mode</button>
+          </Nav> */}
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
