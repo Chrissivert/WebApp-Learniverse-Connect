896fb9b5d977cbf833850447068da0b1e7bd807e
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-02-15 11:20:23 +0100

    Notes from girt's live-coding

--- back-end/src/main/java/no/ntnu/webapp/group01/App.java
+++ back-end/src/main/java/no/ntnu/webapp/group01/App.java
@@ -1,13 +1,52 @@
 package no.ntnu.webapp.group01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
- * Hello world!
- *
+ * The code Girts did in class, or at least what i managed to write down.
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+  private Connection connection;
+  private final String url = ""; //Enter the URL for the DB here!
+
+  private void run() {
+
+  }
+
+  private boolean isConnectionOk() {
+    return connection != null;
+  }
+
+  private void readData() {
+    String query = """
+        ENTER SQL QUERY HERE!
+        """;
+
+    try {
+      PreparedStatement statement = connection.prepareStatement(query);
+      ResultSet rs = statement.executeQuery();
+      System.out.println("Books: ");
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        String title = rs.getString("title");
+        System.out.println("> " + id + " : " + title);
+      }
+    } catch (SQLException sqle) {
+      System.err.println("Error while running query: " + sqle.getMessage());
     }
+  }
+
+  private void openConnectionTool() {
+
+    try {
+      this.connection = DriverManager.getConnection(this.url); 
+      System.out.println("Connection to DB established");
+    } catch (SQLException sqle) {
+      System.err.println("Could not connect to DB: " + sqle.getMessage());
+    }
+  }
 }
