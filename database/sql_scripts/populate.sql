eee6ece1e975de91ae08a626a5a17a17f1fd4cee
chris <chrisssy@stud.ntnu.no>
2024-03-05 19:51:59 +0100

    added the third course to database populateFile

--- database/sql_scripts/populate.sql
+++ database/sql_scripts/populate.sql
@@ -19,19 +19,35 @@ VALUES
           database design, querying data, and basic data manipulation using SQL commands. With a hands-on approach, you''ll engage in 
           practical exercises to reinforce your learning, ensuring you gain the skills necessary to navigate and interact with databases
            confidently. Whether you''re a budding developer, analyst, or anyone eager to harness the power of databases, this course offers
-            an accessible entry point into the world of SQL, setting the stage for your future success in data-driven environments.');
+            an accessible entry point into the world of SQL, setting the stage for your future success in data-driven environments.'),
+
+    (3, 'Creating Web Application with .Net', 'Beginner', '2024-08-05', '2024-08-16', 4, 40,
+        'Embark on your journey into web development with our beginner-level online course, "Creating Web Applications with .NET." 
+        Tailored for those stepping into the dynamic world of web development, this course provides a solid foundation in utilizing the 
+        versatile .NET framework to build powerful and interactive web applications. Guided by experienced instructors, you''ll explore 
+        fundamental concepts such as web application architecture, user interface design, and server-side scripting using .NET technologies 
+        like ASP.NET. Throughout the course, you''ll engage in hands-on projects that walk you through the entire development process, from 
+        designing responsive user interfaces to implementing server-side functionality. Gain practical skills in C# programming and discover
+         how to leverage the robust features of .NET to bring your web applications to life. Whether you''re a programming novice or transitioning
+          from another language, this course offers a welcoming entry point into the exciting realm of web application development with .NET, 
+          setting you on a path to create dynamic and engaging online experiences.');     
 
 INSERT INTO CourseProviders (providerID, providerName)
 VALUES (1, 'Apache Software Foundation'),
        (2, 'NTNU'),
        (3, 'Pearson'),
-       (4, 'Oracle');
+       (4, 'Oracle'),
+       (5, 'Microsoft');
 
 INSERT INTO CourseProviderRelationship (providerID, courseID)
 VALUES (1, 1),  
        (2, 1),  
        (2, 2),
-       (3, 2);
+       (3, 2),
+       (3, 3),
+       (4, 3),
+       (5, 3);
+
 
 INSERT INTO CoursePricing (pricingID, courseID, providerID, price, currency)
 VALUES 
@@ -39,7 +55,10 @@ VALUES
     (2, 1, 4, 32000.00, 'NOK'),
     (3, 2, 2, 800.00, 'USD'),  
     (4, 2, 1, 10000.00, 'NOK'),
-    (5, 2, 3, 899.00, 'USD');
+    (5, 2, 3, 899.00, 'USD'),
+    (6, 3, 5, 2999.00, 'NOK'),
+    (7, 3, 3, 3000.00, 'USD'),
+    (8, 3, 4, 599.00, 'USD');
 
 INSERT INTO CourseCertifications (certificationID, title) 
 VALUES  (1, 'Java SE 17 Programmer Professional'),
