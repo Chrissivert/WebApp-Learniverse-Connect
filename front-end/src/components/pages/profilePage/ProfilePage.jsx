2ed76f4aa5d30081b561b4d0ac04d2c2930b0cd9
Øystein Grande <oystgran@stud.ntnu.no>
2024-03-07 11:00:42 +0100

    Continued working on profile page and added some crude styling

--- front-end/src/components/pages/profilePage/ProfilePage.jsx
+++ front-end/src/components/pages/profilePage/ProfilePage.jsx
@@ -1,12 +1,74 @@
 import React from "react";
 import "./profilePage.css";
 
+const skills = [
+  {
+    courses: "HTML+CSS",
+    level: "advanced",
+    color: "orangered",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "blue",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Java",
+    level: "intermediate",
+    color: "orange",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+];
+
 function ProfilePage() {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profilepage">
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        </div>
+      </div>
     </div>
   );
 }
 
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="front-end/src/resources/images/profile/prince_froggy.png"
+      alt="Prince Froggy"
+    />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Prince Froggy</h1>
+      <p>
+        Full-stack web developer and student at NTNU. I like to play video
+        games, and enjoy swimming at the beach.
+      </p>
+    </div>
+  );
+}
 export default ProfilePage;
