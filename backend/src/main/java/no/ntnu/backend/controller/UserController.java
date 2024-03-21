d001b005b85fe8358d1887350261561b963815fb
birgilv <birgittevik84@gmail.com>
2024-03-21 14:35:13 +0100

    Added get all user method

--- backend/src/main/java/no/ntnu/backend/controller/UserController.java
+++ backend/src/main/java/no/ntnu/backend/controller/UserController.java
@@ -5,10 +5,9 @@ import no.ntnu.backend.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:5173")
@@ -19,6 +18,11 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
 }
