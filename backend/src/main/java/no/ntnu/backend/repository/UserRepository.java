e961b8c790201a9f9d67b9782bf825d9656a71d6
birgilv <birgittevik84@gmail.com>
2024-03-21 14:26:56 +0100

    Added a UserRepository

---
+++ backend/src/main/java/no/ntnu/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package no.ntnu.backend.repository;
+
+import no.ntnu.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
