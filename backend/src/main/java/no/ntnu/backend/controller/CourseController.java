0256c5925395b2c10ec6bf8e1509d3594fc9f0ae
birgilv <birgittevik84@gmail.com>
2024-03-21 14:27:58 +0100

    Added newCourse method

--- backend/src/main/java/no/ntnu/backend/controller/CourseController.java
+++ backend/src/main/java/no/ntnu/backend/controller/CourseController.java
@@ -1,13 +1,26 @@
 package no.ntnu.backend.controller;
 
+import no.ntnu.backend.model.Course;
+import no.ntnu.backend.repository.CourseRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("http://localhost:5173")
 public class CourseController {
     private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @PostMapping("/course")
+    Course newCourse(@RequestBody Course newCourse) {
+        return courseRepository.save(newCourse);
+    }
 
 }
