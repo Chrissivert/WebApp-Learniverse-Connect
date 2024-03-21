01fe9d7f6f861382d3b76243de35e2268ddc003e
birgilv <birgittevik84@gmail.com>
2024-03-21 14:34:31 +0100

    Added user contoller with logger

---
+++ backend/src/main/java/no/ntnu/backend/controller/UserController.java
@@ -0,0 +1,13 @@
+package no.ntnu.backend.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(CourseController.class);
+
+}
