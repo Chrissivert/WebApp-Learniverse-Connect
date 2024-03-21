e2cb82fc6c06da9fc18fe55bbdf5e93fb35eb560
birgilv <birgittevik84@gmail.com>
2024-03-21 14:26:14 +0100

    Added a CourseRepository

---
+++ backend/src/main/java/no/ntnu/backend/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package no.ntnu.backend.repository;
+
+import no.ntnu.backend.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Long> {
+}
