c588d04d1302250bbb71a078124f0fc7f84fa8f9
Øystein Grande <oystgran@stud.ntnu.no>
2024-03-21 13:42:14 +0100

    Import courseboxes to profile page

--- front-end/src/components/pages/profilePage/ProfilePage.jsx
+++ front-end/src/components/pages/profilePage/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./profilePage.css";
+import CourseBox from "../../courseBox/CourseBox";
 
 const skills = [
   {
@@ -47,13 +48,28 @@ function ProfilePage() {
         </div>
       </div>
       <div className="content">
-        <p>This is the content part of the page</p>
+        <div className="contentContainer1">
+          <h3>This is part 1 of the content part of the page</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+            laboriosam laudantium in tempora provident possimus dolorum iusto
+            est, molestias iste optio repellat accusantium ea impedit aperiam
+            praesentium aliquam architecto numquam!
+          </p>
+        </div>
+
+        <h3 className="contentContainer2">
+          This is part 2 of the content part of the page
+        </h3>
       </div>
       <div className="social-media">
         <p>This is miscellaneous social media info about the user</p>
       </div>
       <div className="course-info">
         <p>This is where info about the user's personal courses are located</p>
+        <CourseBox />
+        <CourseBox />
+        <CourseBox />
       </div>
     </div>
   );
@@ -73,9 +89,7 @@ function Intro() {
   return (
     <div>
       <h2>Prince Froggy</h2>
-      <p>
-        Full-stack web developer and student at NTNU. 
-      </p>
+      <p>Full-stack web developer and student at NTNU.</p>
       <p>User since: January 8th 2024</p>
       <div className="buttons">
         <button className="button">Follow</button>
