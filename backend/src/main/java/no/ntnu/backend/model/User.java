5071f586f9b95a751bee121bd212ab150b1ab861
birgilv <birgittevik84@gmail.com>
2024-03-21 14:18:36 +0100

    Added getters and setters to the user entity

---
+++ backend/src/main/java/no/ntnu/backend/model/User.java
@@ -0,0 +1,74 @@
+package no.ntnu.backend.model;
+
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+@Entity
+@Table(name = "user")
+public final class User {
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(int id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
