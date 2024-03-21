5d7ef62b688888d4be56204023dd188e055c2e54
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-03-21 14:04:02 +0100

    Added div files into src folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreateMainPage from './components/pages/mainPage/MainPage.jsx';
+import Footer from "./components/footer/Footer.jsx"
+import './css/index.css';
+import CreatePageNotFound from './components/pages/PageNotFound.jsx';
+import NavigationBar from './components/navbar/Navbar.jsx';
+import ThemeProvider from './components/theme/ThemeProvider.jsx';
+import CourseDetails from './components/pages/coursePage/CoursePage.jsx';
+import CreateSearchCoursesPage from './components/pages/searchCoursePage/SearchCoursePage.jsx';
+import ProfilePage from './components/pages/profilePage/ProfilePage.jsx';
+import AboutUsPage from './components/pages/aboutUsPage/AboutUsPage.jsx';
+import ContactPage from './components/pages/contactPage/ContactPage.jsx';
+import LoginPage from './components/pages/loginPage/Loginpage.jsx';
+import RegisterPage from './components/pages/loginPage/CreateAccountPage.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <NavigationBar/>
+      <Routes>
+        <Route exact path="/" element={<CreateMainPage />} />
+        <Route path="/search" element={<CreateSearchCoursesPage />} />
+        <Route path ="/coursePage" element={<CourseDetails/>} />
+        <Route path="*" element={<CreatePageNotFound/>} />
+        <Route path="/about" element={<AboutUsPage/>} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/contact" element={<ContactPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/createAccount" element={<RegisterPage />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
 }
 
-export default App
+export default App;
