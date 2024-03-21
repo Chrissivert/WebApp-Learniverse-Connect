a7bfb06acf2a7aa0182f3f98c43220361cfa97ef
birgilv <birgittevik84@gmail.com>
2024-03-21 14:35:49 +0100

    Added get update user method

--- backend/src/main/java/no/ntnu/backend/controller/UserController.java
+++ backend/src/main/java/no/ntnu/backend/controller/UserController.java
@@ -31,4 +31,15 @@ public class UserController {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setFirstName(newUser.getFirstName());
+                    user.setLastName(newUser.getLastName());
+                    user.setEmail(newUser.getEmail());
+                    user.setPassword(newUser.getPassword());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
 }
