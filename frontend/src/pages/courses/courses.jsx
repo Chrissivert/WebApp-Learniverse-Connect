aad385625acda209acdecd121e7b172fcd4613c3
birgilv <birgittevik84@gmail.com>
2024-03-22 14:28:23 +0100

    Refactored name from course to courses

---
+++ frontend/src/pages/courses/courses.jsx
@@ -0,0 +1,31 @@
+import './Courses.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import Coursecard from "../../components/coursecard/Coursecard.jsx";
+
+function Courses({}) {
+
+  const [courses, setCourses] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+
+  const loadCourses = async () => {
+    const result = await axios.get("http://localhost:8080/courses");
+    setCourses(result.data);
+  };
+
+  return(
+    <div className="Courses">
+        {courses.map((course, index) => (
+            <Coursecard key={index} course={course} />
+        ))}
+      </div>
+  );
+}
+
+export default Courses;
