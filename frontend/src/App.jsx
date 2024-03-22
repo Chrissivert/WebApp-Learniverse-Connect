a0aac571be21dbec9389f29e4192b28f03184578
birgilv <birgittevik84@gmail.com>
2024-03-22 14:27:34 +0100

    Refactored name from course to courses

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './layouts/footer/Footer.jsx';
 import Home from './pages/home/Home.jsx';
 import About from './pages/about/About.jsx';
 import NotFound from './pages/error/notFound/404.jsx';
-import Course from './pages/course/Course.jsx';
+import Courses from './pages/courses/Courses.jsx';
 import Search from './pages/search/Search.jsx';
 import FImage from '/home/front_image.svg';
 import './index.css';
@@ -18,7 +18,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
-        <Route path='/course' element={<Course description='Test Description' price='Test Price' image={FImage}/>}/>
+        <Route path='/courses' element={<Courses/>}/>
         <Route path='/search' element={<Search/>}/>
         <Route path='*' element={<NotFound/>}/>
         <Route path='profile' element={<Profile/>}/>
