28a0d710c701658206242188948c56a9c6dc52ab
birgilv <birgittevik84@gmail.com>
2024-03-22 14:50:28 +0100

    Added go back button to courses

--- frontend/src/pages/course/Course.jsx
+++ frontend/src/pages/course/Course.jsx
@@ -1,29 +1,19 @@
 import './Course.css';
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link, useParams } from "react-router-dom";
-import Coursecard from "../../components/coursecard/Coursecard.jsx";
+import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Course({}) {
 
-  const [courses, setCourses] = useState([]);
 
-  const { id } = useParams();
-
-  useEffect(() => {
-    loadCourses();
-  }, []);
-
-  const loadCourses = async () => {
-    const result = await axios.get("http://localhost:8080/courses");
-    setCourses(result.data);
-  };
 
   return(
-    <div className="Courses">
-        {courses.map((course, index) => (
-            <Coursecard key={index} course={course} />
-        ))}
+    <div className="Course">
+        <h1>This is course</h1>
+        <Link to = {'/courses'}>
+          <button>Go back</button>
+        </Link>
+        
       </div>
   );
 }
