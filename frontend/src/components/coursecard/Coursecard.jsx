bdd229d742efc33c74b41fe73c51b0d30340546f
birgilv <birgittevik84@gmail.com>
2024-03-22 11:27:57 +0100

    Refactored information showed on course card

--- frontend/src/components/coursecard/Coursecard.jsx
+++ frontend/src/components/coursecard/Coursecard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import './Coursecard.css'
 
 function Coursecard({ course }) {
   return (
     <div className="coursecard">
       <div className="card-body">
         <h5 className="card-title">{course.title}</h5>
-        <p className="card-text">ID: {course.id}</p>
-        <p className="card-text">Duration: {course.duration}</p>
-        <p className="card-text">Description: {course.description}</p>
+        <p className="card-text">Start Date: {course.startDate}</p>
+        <p className="card-text">Related Certification: {course.relatedCertification}</p>
       </div>
     </div>
   );
