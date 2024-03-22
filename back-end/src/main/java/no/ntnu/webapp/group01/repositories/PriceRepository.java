4be1516339c5e3a2a2f50f03132097780bcd5bd0
chris <chrisssy@stud.ntnu.no>
2024-03-22 23:01:15 +0100

    added duplicated back-end

--- back-end/src/main/java/no/ntnu/webapp/group01/repositories/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package no.ntnu.webapp.group01.repositories;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import no.ntnu.webapp.group01.models.CoursePrice;
-
-public interface PriceRepository extends JpaRepository<CoursePrice, Long> {
-    List<CoursePrice> findByOrderByPriceAsc();
-}
--- back-end/src/main/java/no/ntnu/webapp/group01/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-// package no.ntnu.webapp.group01.repositories;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import no.ntnu.webapp.group01.models.Role;
-
-// public interface RoleRepository extends JpaRepository<Role, Long> {
-//   Role findOneByName(String name);
-// }
