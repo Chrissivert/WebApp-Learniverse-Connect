187a50e9136236f67000d10e93e4c71a1eae4261
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-03-22 12:17:30 +0100

    Added provider entity

---
+++ backend/src/main/java/no/ntnu/backend/model/Provider.java
@@ -0,0 +1,62 @@
+package no.ntnu.backend.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Provider {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  private String name;
+
+  public Provider() {
+  }
+
+  public Provider(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Provider) obj;
+        return this.id == that.id &&
+                Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "User[" +
+                "id=" + id + ", " +
+                "name=" + name + "]";
+    }
+}
