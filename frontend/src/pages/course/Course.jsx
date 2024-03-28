fb956997cec894950bdf50eaaca8f105ef9f3f66
chris <chrisssy@stud.ntnu.no>
2024-03-28 02:50:03 +0100

    added fade in styling in course

--- frontend/src/pages/course/Course.jsx
+++ frontend/src/pages/course/Course.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import "./Course.css";
 
 function Course() {
   const { id } = useParams();
