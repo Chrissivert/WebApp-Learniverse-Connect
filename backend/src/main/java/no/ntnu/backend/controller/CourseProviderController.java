66833d84bfcc38f9f949324a9c1a81aa484627d2
chris <chrisssy@stud.ntnu.no>
2024-03-30 02:26:03 +0100

    added a DTO for courseProviders

--- backend/src/main/java/no/ntnu/backend/controller/CourseProviderController.java
+++ backend/src/main/java/no/ntnu/backend/controller/CourseProviderController.java
@@ -5,20 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import no.ntnu.backend.dto.CourseByEachProviderDTO;
 import no.ntnu.backend.model.CourseProvider;
-import no.ntnu.backend.service.CourseProviderService;
+import no.ntnu.backend.service.CourseProviderServiceDTO;
 
 @RestController
 @CrossOrigin("http://localhost:5173")
 public class CourseProviderController {
 
     @Autowired
-    private CourseProviderService courseProviderService;
+    private CourseProviderServiceDTO courseProviderService;
+
+    @GetMapping("/course-by-each-provider")
+    public List<CourseByEachProviderDTO> getAllCourseProvidersWithProviderNames() {
+        return courseProviderService.getAllCourseProvidersWithProviderNames("NOK");
+    }
 
     @GetMapping("/cheapest-course-prices")
     public List<CourseProvider> getCheapestCoursePrices() {
-        return courseProviderService.getConvertedCoursePrices("NOK");
+        return courseProviderService.getCheapestCoursePrices("NOK");
     }
 }
