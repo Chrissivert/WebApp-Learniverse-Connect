f19b384d7f38a456780560d93a3ba763dd26cd81
chris <chrisssy@stud.ntnu.no>
2024-04-16 12:48:48 +0200

    removed duplicate frontend folder again

---
+++ frontend/src/SortButtons.jsx
@@ -0,0 +1,19 @@
+// // SortButtons.js
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// function SortButtons({ handleSortBy }) {
+//   return (
+//     <div className="sort-buttons">
+//       <button onClick={() => handleSortBy('credits')}>Sort by Credits</button>
+//       <button onClick={() => handleSortBy('title')}>Sort by Title</button>
+//       {/* Add more buttons for other sorting options */}
+//     </div>
+//   );
+// }
+
+// SortButtons.propTypes = {
+//     handleSortBy: PropTypes.func.isRequired,
+//   };
+
+// export default SortButtons;
