4f9d50f2af7e662594ca80f1a854d21d5ddeea5b
birgilv <birgittevik84@gmail.com>
2024-04-22 12:38:49 +0200

    Added javadoc to config and main

--- backend/src/main/java/no/ntnu/backend/config/CorsConfig.java
+++ backend/src/main/java/no/ntnu/backend/config/CorsConfig.java
@@ -6,11 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class for CORS (Cross-Origin Resource Sharing) in the Spring application.
+ *
+ * @author Group 01
+ * @version 23.05.2024
+ */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
 
+    /**
+     * Adds CORS mappings to the provided registry.
+     *
+     * @param registry The CORS registry to which mappings are added.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         logger.info("Configuring CORS...");
