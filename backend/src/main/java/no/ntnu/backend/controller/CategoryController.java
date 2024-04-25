210538b553c7cd3f87d5713e5d5ed5d519d514b1
chris <chrisssy@stud.ntnu.no>
2024-04-25 14:45:04 +0200

    added api doc for remaining controller classes

--- backend/src/main/java/no/ntnu/backend/controller/CategoryController.java
+++ backend/src/main/java/no/ntnu/backend/controller/CategoryController.java
@@ -2,6 +2,7 @@ package no.ntnu.backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import no.ntnu.backend.model.Category;
 import no.ntnu.backend.service.CategoryService;
 
@@ -9,92 +10,47 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
-/**
- * Controller class for handling CRUD operations related to categories.
- * Handles HTTP requests/responses for category-related endpoints.
- *
- * @author Group 01
- * @version 23.05.2024
- */
 @RestController
 @RequestMapping("/categories")
 @CrossOrigin("http://localhost:5173")
 public class CategoryController {
 
-  private final CategoryService categoryService;
+    private final CategoryService categoryService;
 
-  /**
-   * Constructor for CategoryController.
-   *
-   * @param categoryService The CategoryService to be injected.
-   */
-  @Autowired
-  public CategoryController(CategoryService categoryService){
-    this.categoryService = categoryService;
+    @Autowired
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
 
-  }
+    @Operation(summary = "Creates a new category", description = "Creates a new category.")
+    @PostMapping()
+    public ResponseEntity<String> createCategory(@RequestBody Category category) {
+        return this.categoryService.create(category);
+    }
 
-  /**
-   * Creates a new category.
-   *
-   * @param category The category object to be created.
-   * @return ResponseEntity indicating the success/failure of the operation.
-   */
-  @PostMapping()
-  public ResponseEntity<String> createCategory(@RequestBody Category category) {
-    return this.categoryService.create(category);
-  }
+    @Operation(summary = "Retrieves all categories", description = "Retrieves all categories.")
+    @GetMapping()
+    public List<Category> readAllCategories() {
+        return this.categoryService.readAll();
+    }
 
-  /**
-   * Retrieves all categories.
-   *
-   * @return List of all categories.
-   */
-  @GetMapping()
-  public List<Category> readAllCategories() {
-    return this.categoryService.readAll();
-  }
+    @Operation(summary = "Retrieves a category by its ID", description = "Retrieves a category by its ID.")
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> readCategoryById(@PathVariable int id) {
+        return this.categoryService.readById(id);
+    }
 
-  /**
-   * Retrieves a category by its ID.
-   *
-   * @param id The ID of the category to retrieve.
-   * @return ResponseEntity containing the requested category, if found.
-   */
-  @GetMapping("/{id}")
-  public ResponseEntity<Category> readCategoryById(@PathVariable int id) {
-    return this.categoryService.readById(id);
-  }
+    @Operation(summary = "Updates an existing category", description = "Updates an existing category.")
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable int id, @RequestBody Category category) {
+        return this.categoryService.update(id, category);
+    }
 
-  /**
-   * Updates an existing category.
-   *
-   * @param id       The ID of the category to be updated.
-   * @param category The updated category object.
-   * @return ResponseEntity indicating the success/failure of the operation.
-   */
-  @PutMapping("/{id}")
-  public ResponseEntity<String> updateCategory(@PathVariable int id, @RequestBody Category category) {
-    return this.categoryService.update(id, category);
-  }
-
-  /**
-   * Deletes a category by its ID.
-   *
-   * @param id The ID of the category to be deleted.
-   * @return ResponseEntity indicating the success/failure of the operation.
-   */
-  @DeleteMapping("/{id}")
-  public ResponseEntity<String> deleteCategory(@PathVariable int id) {
-    return this.categoryService.delete(id);
-  }
+    @Operation(summary = "Deletes a category by its ID", description = "Deletes a category by its ID.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable int id) {
+        return this.categoryService.delete(id);
+    }
 }
--- backend/src/main/java/no/ntnu/backend/controller/CourseTagsController.java
+++ backend/src/main/java/no/ntnu/backend/controller/CourseTagsController.java
@@ -1,48 +1,28 @@
+
 package no.ntnu.backend.controller;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
 import no.ntnu.backend.model.CourseTags;
 import no.ntnu.backend.repository.CourseTagsRepository;
 
-/**
- * Controller class for managing operations related to course tags.
- * Handles HTTP requests/responses for course tags-related endpoints.
- *
- * @author Group 01
- * @version 23.05.2024
- */
 @RestController
 @CrossOrigin("http://localhost:5173")
 public class CourseTagsController {
 
     private final CourseTagsRepository courseTagsRepository;
 
-    /**
-     * Constructor for CourseTagsController.
-     *
-     * @param courseTagsRepository The CourseTagsRepository to be injected.
-     */
     @Autowired
     public CourseTagsController(CourseTagsRepository courseTagsRepository){
         this.courseTagsRepository = courseTagsRepository;
-
     }
 
-    /**
-     * Retrieves all course tags.
-     *
-     * @return List of CourseTags containing information about all course tags.
-     */
+    @Operation(summary = "Retrieves all course tags",
+               description = "Retrieves all course tags.")
     @GetMapping("/course-tags")
     List<CourseTags> getAllCourses() {
         return courseTagsRepository.findAll();
     }
 }
-
-
