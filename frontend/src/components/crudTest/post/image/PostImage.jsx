d9252657f6f01b37d48a2cfe88a51c2c64b3ffdb
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-02 10:46:32 +0200

    Can add alttext when uploading image in frontend

--- frontend/src/components/crudTest/post/image/PostImage.jsx
+++ frontend/src/components/crudTest/post/image/PostImage.jsx
@@ -3,16 +3,30 @@ import axios from 'axios';
 
 export default function PostImage() {
   const [file, setFile] = useState(null);
+  const [altText, setAltText] = useState("");
   const [image, setImage] = useState(null);
 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
+    setImage(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleAltTextChange = (e) => {
+    setAltText(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (!file || !altText) {
+      console.error("Please select a file and enter an image description.")
+      return;
+    }
+
     const formData = new FormData();
     formData.append('file', file);
+    formData.append('alt', altText)
+
     try {
       await axios.post(`http://localhost:8080/images`, formData, {
         headers: {
@@ -28,10 +42,17 @@ export default function PostImage() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="file" name="file" onChange={handleFileChange}/>
+        <label>
+          Upload File
+          <input type="file" name="file" onChange={handleFileChange}/>
+        </label><br/>
+        <label>
+          Image Description
+          <input value={altText} onChange={handleAltTextChange}/>
+        </label><br/>
+        <img src={image} width={300}/>
         <button type='submit'>Upload</button>
       </form>
-      <img/>
     </div>
   )
 }
