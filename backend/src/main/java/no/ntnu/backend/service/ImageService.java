b8f8ece4a8232caf3eec172405e90db553cb98f9
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-02 09:05:07 +0200

    Created the remaining logic for the Update and Delete operations for Image handling

--- backend/src/main/java/no/ntnu/backend/service/ImageService.java
+++ backend/src/main/java/no/ntnu/backend/service/ImageService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
 import no.ntnu.backend.model.Image;
@@ -40,7 +41,7 @@ public class ImageService {
     return this.imageRepository.findAll();
   }
 
-  public ResponseEntity<Image> readById(Integer id) {
+  public ResponseEntity<Image> readById(int id) {
     ResponseEntity<Image> response;
 
     Image image = this.getImageById(id);
@@ -53,6 +54,65 @@ public class ImageService {
     return response;
   }
 
+  public ResponseEntity<String> update(@PathVariable int id, MultipartFile file) {
+    ResponseEntity<String> response;
+
+    try {
+      Image image = new Image();
+      image.setFileName(file.getOriginalFilename());
+      image.setData(file.getBytes());
+      image.setContentType(file.getContentType());
+      this.updateImage(id, image);
+      response = new ResponseEntity<>(image.toString(), HttpStatus.OK);
+    } catch (Exception e) {
+      response = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    return response;
+  }
+
+  public ResponseEntity<String> delete(@PathVariable int id) {
+    ResponseEntity<String> response;
+
+    if (this.removeImage(id)) {
+      response = new ResponseEntity<>("Couse with ID: " + id + "has been deleted", HttpStatus.OK);
+    } else {
+      response = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    return response;
+  }
+
+  private boolean removeImage(int id) {
+    boolean result = false;
+
+    try {
+      this.imageRepository.delete(this.getImageById(id));
+      result = true;
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Invalid ID");
+    }
+
+    return result;
+  }
+
+  private void updateImage(int id, Image image) {
+    Image existingImage = this.getImageById(id);
+
+    if (existingImage == null) {
+      throw new IllegalArgumentException();
+    }
+    if (image == null || !image.isValid()) {
+      throw new IllegalArgumentException();
+    }
+    if ((image.getId()) != id) {
+      throw new IllegalArgumentException();
+    }
+
+    image.setId(existingImage.getId());
+    this.imageRepository.save(image);
+  }
+
   private void addImage(Image image) throws IllegalArgumentException {
     if (image == null) {
       throw new IllegalArgumentException("Image is invalid");
@@ -61,7 +121,7 @@ public class ImageService {
     this.imageRepository.save(image);
   } 
 
-  public Image getImageById(Integer id) {
+  public Image getImageById(int id) {
     return this.imageRepository.findById(id).get();
   }
 }
