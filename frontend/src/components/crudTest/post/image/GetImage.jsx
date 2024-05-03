67b60e6e6a10cf34cf60e849511aa01357f13c88
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-03 13:41:20 +0200

    Component that gets the 'raw' data from databse as an byte[] to blob, and convert it to image again

--- frontend/src/components/crudTest/post/image/GetImage.jsx
+++ frontend/src/components/crudTest/post/image/GetImage.jsx
@@ -1,10 +1,28 @@
-export default function GetImage() {
-  //const [image, setImage] = useState("");
+import axios from "axios";
+import { useEffect, useState } from "react"
 
+export default function GetImage({imageId}) {
+  const [imageUrl, setImageUrl] = useState('');
+  const [imageContentType, setImageContentType] = useState('');
+
+  useEffect(() => {
+    axios.get(`http://localhost:8080/images/${imageId}/data`, {
+      responseType: 'arraybuffer'
+    }).then(response => {
+      const contentType = response.headers['content-type'];
+      const blob = new Blob([response.data], {type: contentType});
+      setImageUrl(URL.createObjectURL(blob));
+      setImageContentType(contentType);
+    }).catch(error => {
+      console.error("error fetching image data:", error);
+    });
+  }, [imageId]);
 
   return (
-    <>
-
-    </>
+    <div>
+      {/* {imageUrl && <img src={imageUrl} alt="Image"/>} */}
+      <img src={imageUrl} width={300}/>
+      {imageContentType && <p>Content Type: {imageContentType}</p>}
+    </div>
   )
 }
