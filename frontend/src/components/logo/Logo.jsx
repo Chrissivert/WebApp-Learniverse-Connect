04805998dc5808bd1dcb00b6cc758f3d59322ef8
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-03 11:28:35 +0200

    changed from 'a' reference method to 'Link' method from react-router-dom

--- frontend/src/components/logo/Logo.jsx
+++ frontend/src/components/logo/Logo.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import './Logo.css';
 import LogoImage from '/logo/white_icon.png';
 
 export default function Logo({home_src = false}) {
   return (
     <div className='logo-container'>
-      <a href={home_src ? '/' : null}>
+      <Link to={home_src ? '/' : null}>
         <img src={LogoImage} alt='Logo' className='logo' id='image'/>
-      </a>
+      </Link>
     </div>
   );
 }
