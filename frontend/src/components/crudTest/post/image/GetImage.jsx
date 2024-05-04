2406d00b08eb50067f1aab85f51fe5bf3fff9833
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-04 10:09:33 +0200

    Commented out unnecesary code

--- frontend/src/components/crudTest/post/image/GetImage.jsx
+++ frontend/src/components/crudTest/post/image/GetImage.jsx
@@ -1,28 +1,55 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
 
+/**
+ * 
+ *
+ * @param {int} param0 
+ * @returns 
+ */
 export default function GetImage({imageId}) {
-  const [imageUrl, setImageUrl] = useState('');
-  const [imageContentType, setImageContentType] = useState('');
+  // const [imageUrl, setImageUrl] = useState('');
+  // const [imageContentType, setImageContentType] = useState('');
+  const [altText, setAltText] = useState('');
+
+  /**
+   * Gets images from database.
+   */
+  // function getImage() {
+  //   axios.get(`http://localhost:8080/images/${imageId}/data`, {
+  //     responseType: 'arraybuffer'
+  //   }).then(response => {
+  //     const contentType = response.headers['content-type'];
+  //     const blob = new Blob([response.data], {type: contentType});
+  //     setImageUrl(URL.createObjectURL(blob));
+  //     setImageContentType(contentType);
+  //   }).catch(error => {
+  //     console.error("Error fetching image data:", error);
+  //   });
+  // }
+
+  /**
+   * Gets alt-text from database
+   */
+  function getAltText() {
+    axios.get(`http://localhost:8080/images/${imageId}`)
+    .then(response => {
+      const alt = response.data.alt;
+      setAltText(alt);
+    }).catch(error => {
+      console.error("Error fetching alt data:", error);
+    });
+  }
 
   useEffect(() => {
-    axios.get(`http://localhost:8080/images/${imageId}/data`, {
-      responseType: 'arraybuffer'
-    }).then(response => {
-      const contentType = response.headers['content-type'];
-      const blob = new Blob([response.data], {type: contentType});
-      setImageUrl(URL.createObjectURL(blob));
-      setImageContentType(contentType);
-    }).catch(error => {
-      console.error("error fetching image data:", error);
-    });
+    // getImage()
+    getAltText();
   }, [imageId]);
 
   return (
     <div>
-      {/* {imageUrl && <img src={imageUrl} alt="Image"/>} */}
-      <img src={imageUrl} width={300}/>
-      {imageContentType && <p>Content Type: {imageContentType}</p>}
+      <img src={`http://localhost:8080/images/${imageId}/data`} width={300} alt={altText}/>
+      {/* {imageContentType && <p>Content Type: {imageContentType}</p>} */}
     </div>
   )
 }
