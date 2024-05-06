a617fb7044b3dfb645335da406797494d1b22be1
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-06 09:50:18 +0200

    Deleted unused code

--- frontend/src/components/crudTest/post/image/GetImage.jsx
+++ frontend/src/components/crudTest/post/image/GetImage.jsx
@@ -4,29 +4,11 @@ import { useEffect, useState } from "react"
 /**
  * 
  *
- * @param {int} param0 
+ * @param {int} imageId 
  * @returns 
  */
 export default function GetImage({imageId}) {
-  // const [imageUrl, setImageUrl] = useState('');
-  // const [imageContentType, setImageContentType] = useState('');
   const [altText, setAltText] = useState('');
-
-  /**
-   * Gets images from database.
-   */
-  // function getImage() {
-  //   axios.get(`http://localhost:8080/images/${imageId}/data`, {
-  //     responseType: 'arraybuffer'
-  //   }).then(response => {
-  //     const contentType = response.headers['content-type'];
-  //     const blob = new Blob([response.data], {type: contentType});
-  //     setImageUrl(URL.createObjectURL(blob));
-  //     setImageContentType(contentType);
-  //   }).catch(error => {
-  //     console.error("Error fetching image data:", error);
-  //   });
-  // }
 
   /**
    * Gets alt-text from database
@@ -42,14 +24,12 @@ export default function GetImage({imageId}) {
   }
 
   useEffect(() => {
-    // getImage()
     getAltText();
   }, [imageId]);
 
   return (
     <div>
       <img src={`http://localhost:8080/images/${imageId}/data`} width={300} alt={altText}/>
-      {/* {imageContentType && <p>Content Type: {imageContentType}</p>} */}
     </div>
   )
 }
