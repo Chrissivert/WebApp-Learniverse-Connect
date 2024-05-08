438b07938cd4de348c23fe5ec29054e8fb05013c
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-08 22:43:53 +0200

    Refectoring, added posibility to do crud operations on BLOB and JSON with the same default method 'sendApuRequest()'

--- frontend/src/services/api-requests.jsx
+++ frontend/src/services/api-requests.jsx
@@ -1,9 +1,30 @@
 import axios from "axios";
 
 /**
- * The API key for the base url for the website.
+ * The base url for the website.
  */
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+/**
+ * CRUD REST-API Request methods.
+ */
+const apiRequestMethods = Object.freeze({
+  POST: 'post',
+  GET: 'get',
+  PUT: 'put',
+  DELETE: 'delete'
+});
+
+/**
+ * Sends an HTTP POST REST-API request to the backend.
+ * 
+ * @param {string} url The URL that will be used for the request.
+ * @param {FormData} data The data to be sent with the request.
+ * @returns The response from the request.
+ */
+export async function sendApiPostRequest(url, data) {
+  return sendApiRequest(apiRequestMethods.POST, url, data);
+}
 
 /**
  * Sends an HTTP GET REST-API request to the backend.
@@ -12,29 +33,18 @@ export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
  * @returns The response from the request.
  */
 export async function sendApiGetRequest(url) {
-  return sendApiJsonRequest('get', url);
-}
-
-/**
- * Sends an HTTP POST REST-API request to the backend.
- * 
- * @param {string} url The URL that will be used for the request.
- * @param {json} requestBody The JSON data to be sent with the request.
- * @returns The response from the request.
- */
-export async function sendApiPostRequest(url, requestBody) {
-  return sendApiJsonRequest('post', url, requestBody);
+  return sendApiRequest(apiRequestMethods.GET, url);
 }
 
 /**
  * Sends an HTTP PUT REST-API request to the backend.
  * 
  * @param {string} url The URL that will be used for the request.
- * @param {json} requestBody The JSON data to be sent with the request.
+ * @param {FormData} data The data to be sent with the request.
  * @returns The response from the request.
  */
-export async function sendApiPutRequest(url, requestBody) {
-  return sendApiJsonRequest('put', url, requestBody);
+export async function sendApiPutRequest(url, data) {
+  return sendApiRequest(apiRequestMethods.PUT, url, data);
 }
 
 /**
@@ -44,18 +54,7 @@ export async function sendApiPutRequest(url, requestBody) {
  * @returns The response from the request.
  */
 export async function sendApiDeleteRequest(url) {
-  return sendApiJsonRequest('delete', url);
-}
-
-/**
- * 
- * 
- * @param {string} url The URL that will be used for the request.
- * @param {byte[]} file
- * @returns The response from the request.
- */
-export async function sendApiFileUploadRequest(url, file) {
-  // sendApiRequest("post", url, callback, null, errorCallback, fileContent);
+  return sendApiRequest(apiRequestMethods.DELETE, url);
 }
 
 /**
@@ -63,50 +62,27 @@ export async function sendApiFileUploadRequest(url, file) {
  *
  * @param {string} method The request method to be used when making the request. Methods to be used: POST, GET, PUT, DELETE.
  * @param {string} url The URL that will be used for the request.
- * @param {json} requestBody The JSON data to be sent with the request.
- * @param {byte[]} file The BLOB data to be sent with the request.
+ * @param {FormData} data The data to be sent with the request.
  * @returns The response from the request.
  */
-async function sendApiJsonRequest(method, url, requestBody) {
+async function sendApiRequest(method, url, data) {
   try {
     const config = {
-      method: method,
+      method: method.toLowerCase(),
       baseURL: API_BASE_URL,
       url: url,
-      data: (method === 'get' || 'delete') ? null : requestBody,
-    };
+      data: (method === apiRequestMethods.POST || apiRequestMethods.PUT) ? data : null,
+      headers: (url.includes('images') ? ({
+        'Content-Type': 'multipart/form-data'
+      }) : null // ({
+      //  'Content-Type': 'application/json'
+      //})
+    )};
 
     const response = await axios(config);
+    console.log("API request sent succesfully");
     return response;
   } catch (error) {
-    console.error("Error in sendApiJsonRequest:", error);
-  }
-};
-
-/**
- * 
- * @param {string} method 
- * @param {string} url 
- * @param {formData} file 
- */
-async function sendApiBlobRequest(method, url, file) {
-  try {
-    const config = {
-      method: method,
-      baseURL: API_BASE_URL,
-      url: url,
-      responseType: 'blob'
-    };
-
-    if (method === 'post') {
-      await axios.post(config.baseURL + config.url, file, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-      console.log("File uploaded successfully");
-    }
-  } catch (error) {
-    console.error("Error in sendApiBlobRequest:", error);
+    console.error("Error in sendApiRequest:", error);
   }
 }
