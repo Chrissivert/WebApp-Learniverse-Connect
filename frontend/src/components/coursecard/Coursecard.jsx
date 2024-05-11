6a718559855b2365b0ae9d3dcb3a1e39e5449c26
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-11 12:13:09 +0200

    Added image to coursecard

--- frontend/src/components/coursecard/Coursecard.jsx
+++ frontend/src/components/coursecard/Coursecard.jsx
@@ -1,41 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Coursecard.css";
-import { loadImage } from "../../functions/ImageLoader"; // Adjusted the import path
+import GetImage from "../crudTest/post/image/GetImage";
 
-function Coursecard({ course }) {
-  const [imageUrl, setImageUrl] = useState(null);
-
-  useEffect(() => {
-    loadImage(course.title)
-      .then(url => setImageUrl(url))
-      .catch(error => console.error("Error loading image:", error));
-  }, [course.title]);
-
-  const { cheapestPrice, cheapestPriceCurrency } = course;
-  const roundedCheapestPrice = cheapestPrice ? cheapestPrice.toFixed(0) : null;
-
-  // Log roundedCheapestPrice and course
-  useEffect(() => {
-    console.log("roundedCheapestPrice:", roundedCheapestPrice);
-    console.log("course:", course);
-    console.log("imageUrl:", imageUrl);
-    console.log("title:", course.title);
-    console.log("credit:", course.credit);
-    console.log("cheapestPrice:", cheapestPrice);
-    console.log("cheapestPriceCurrency:", cheapestPriceCurrency);
-  }, [roundedCheapestPrice, course, imageUrl]);
+export default function Coursecard({ course }) {
 
   return (
     <div className="course-card">
-      <div className="image-section" style={{backgroundImage: `url(${imageUrl})`}}>
+      <div className="image-section">
+        <GetImage imageId={course.id}/>
       </div>
-      <div className="text-section">
-        <h2>Title: {course.title}</h2>
-        <p>Credits: {course.credit}</p>
-        {roundedCheapestPrice && <p className="cheapest-price">Cheapest Price: {roundedCheapestPrice} {cheapestPriceCurrency}</p>}
+      <div className="info-section">
+        <h2>{course.title}</h2>
+        <p>Start Date: {course.startDate}</p>
       </div>
     </div>
-  );
+  )
 }
-
-export default Coursecard;
