8fadc2cb06739551c782ccc133f224b87a9283a4
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-13 12:45:20 +0200

    Added the rest of the CRUD methods for user-service

--- frontend/src/services/user-request.jsx
+++ frontend/src/services/user-request.jsx
@@ -1,6 +1,25 @@
 import { sendApiPostRequest, sendApiGetRequest, sendApiPutRequest, sendApiDeleteRequest } from "./api-requests";
 
-const PROFILE_URL = "/user"
+const USER_URL = "/user";
+
+/**
+ * Adds a user to the server.
+ *
+ * @param {FormData} user The user to be added.
+ * @returns The outcome of the post request.
+ */
+export function addUserToServer(user) {
+  return sendApiPostRequest(USER_URL, user);
+}
+
+/**
+ * Retrieves the users from the server.
+ *
+ * @returns The outcome of the get request.
+ */
+export function getUsersFromServer() {
+  return sendApiGetRequest(USER_URL);
+}
 
 /**
  * Retrieves a user from the server.
@@ -9,10 +28,31 @@ const PROFILE_URL = "/user"
  * @returns The outcome of the get request.
  */
 export function getUserFromServer(userId) {
-  return sendApiGetRequest(PROFILE_URL + "/" + userId)
+  return sendApiGetRequest(USER_URL + "/" + userId);
 }
 
-const REGISTER_URL = "/api/signup"
+/**
+ * Updates a user on the server.
+ *
+ * @param {int} userId The ID of the user to be updated.
+ * @param {FormData} user The updated user to be sent.
+ * @returns The outcome of the put request.
+ */
+export function updateUserOnServer(userId, user) {
+  return sendApiPutRequest(USER_URL + "/" + userId, user);
+}
+
+/**
+ * Deletes a user from the server.
+ *
+ * @param {int} userId The ID of the user to be deleted. 
+ * @returns The outcome of the delete request.
+ */
+export function deleteUserOnServer(userId) {
+  return sendApiDeleteRequest(USER_URL + "/" + userId);
+}
+
+const REGISTER_URL = "/api/signup";
 
 /**
  * 
@@ -24,7 +64,7 @@ export function postSignupToServer(data) {
   return sendApiPostRequest(REGISTER_URL, data);
 }
 
-const AUTH_URL = "/api/authenticate"
+const AUTH_URL = "/api/authenticate";
 
 /**
  * 
