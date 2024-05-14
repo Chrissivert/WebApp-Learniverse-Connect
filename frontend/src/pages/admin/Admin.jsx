b352f272a47d35159058d487a892a0b226ee836b
birgilv <birgittevik84@gmail.com>
2024-05-14 11:31:56 +0200

    User is checked if admin

--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -8,9 +8,13 @@ function AdminPage() {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        console.log(auth.user) // Check if user have been loaded
         // Check if the user has the admin role
-        if (auth.user && auth.user.roles && auth.user.roles.includes('ADMIN')) {
-            setIsAdmin(true);
+        if (auth.user && auth.user.roles && Array.isArray(auth.user.roles)) {
+            console.log("User roles:", auth.user.roles);
+            const isAdmin = auth.user.roles.some(role => role.authority === 'Admin');
+            console.log("Is admin:", isAdmin);
+            setIsAdmin(isAdmin);
         }
         setLoading(false);
     }, [auth.user]);
