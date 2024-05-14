02c31a075a5d86ff71c72264251c13d8faad93ea
Øystein Grande <oystgran@stud.ntnu.no>
2024-05-14 16:49:02 +0200

    Added possiblity to send a user get request with the users email

--- frontend/src/services/user-request.jsx
+++ frontend/src/services/user-request.jsx
@@ -1,36 +1,41 @@
-import { sendApiPostRequest, sendApiGetRequest, sendApiPutRequest, sendApiDeleteRequest } from "./api-requests";
+import {
+  sendApiPostRequest,
+  sendApiGetRequest,
+  sendApiPutRequest,
+  sendApiDeleteRequest,
+} from "./api-requests";
 
-const PROFILE_URL = "/user"
+const PROFILE_URL = "/user";
 
 /**
  * Retrieves a user from the server.
  *
- * @param {int} userId The ID of the user to be retrieved.
+ * @param {String} email The email of the user to be retrieved.
  * @returns The outcome of the get request.
  */
-export function getUserFromServer(userId) {
-  return sendApiGetRequest(PROFILE_URL + "/" + userId)
+export function getUserFromServer(email) {
+  return sendApiGetRequest(PROFILE_URL + "/" + email);
 }
 
-const REGISTER_URL = "/api/signup"
+const REGISTER_URL = "/api/signup";
 
 /**
- * 
  *
- * @param {FormData} data 
- * @returns 
+ *
+ * @param {FormData} data
+ * @returns
  */
 export function postSignupToServer(data) {
   return sendApiPostRequest(REGISTER_URL, data);
 }
 
-const AUTH_URL = "/api/authenticate"
+const AUTH_URL = "/api/authenticate";
 
 /**
- * 
  *
- * @param {FormData} data 
- * @returns 
+ *
+ * @param {FormData} data
+ * @returns
  */
 export function postAuthToServer(data) {
   return sendApiPostRequest(AUTH_URL, data);
