e6587f7afff666b200eb936aa11b1dfe6e567552
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-16 07:03:43 +0200

    Added content to the footer

--- frontend/src/layouts/footer/Footer.jsx
+++ frontend/src/layouts/footer/Footer.jsx
@@ -1,12 +1,16 @@
 import './Footer.css';
+import ntnuLogo from '/footer/Logo-Ntnu.png';
 
 export default function Footer() {
   return (
     <footer>
-      This is a footer
-      <br/>This is a br in a footer
-      <div>
-        <br/>This is a br in a div in a footer
+      <div className='ntnu-logo'>
+        <img src={ntnuLogo} width={100}/>
+      </div>
+      <div className='disclaimer'>
+        This website is a result of a university group project, performed in the course <a target="_blank" href='https://www.ntnu.edu/studies/courses/IDATA2301#tab=omEmnet'>IDATA2301 Web 
+        technologies</a>, at <a target="_blank" href='https://www.ntnu.edu/'>NTNU</a>. All the information provided here is a result of imagination. Any 
+        resemblance with real companies or products is a coincidence.
       </div>
     </footer>
   );
