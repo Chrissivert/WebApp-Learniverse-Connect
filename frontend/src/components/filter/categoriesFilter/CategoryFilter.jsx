b0b0184957bbb56be1ad25cc34fc35f39f4b1215
chris <chrisssy@stud.ntnu.no>
2024-05-16 13:20:29 +0200

    added remove category option

--- frontend/src/components/filter/categoriesFilter/CategoryFilter.jsx
+++ frontend/src/components/filter/categoriesFilter/CategoryFilter.jsx
@@ -12,7 +12,7 @@ export default function CategoryFilter({ onCategoryChange }) {
   };
 
   /**
-   * Fetches the category-names in a list
+   * Fetches the category names in a list
    */
   async function fetchCategories() {
     const response = await getCategoriesFromServer();
@@ -23,11 +23,17 @@ export default function CategoryFilter({ onCategoryChange }) {
     fetchCategories();
   }, []);
 
+  const clearSelection = () => {
+    setSelectedCategory('');
+    onCategoryChange(''); // Notify parent component that the category has been cleared
+  };
+
   return (
     <div>
-      <label htmlFor="categorySelect"></label>
+      <label htmlFor="categorySelect">Category:</label>
       <select id="categorySelect" className="category-select" onChange={handleCategoryChange} value={selectedCategory}>
-        <option value="">-- Select a category --</option>
+        {!selectedCategory && <option value="">-- Select a category --</option>}
+        {selectedCategory && <option value="" style={{ fontWeight: 'bold' }}>-- REMOVE CATEGORY --</option>}
         {categories.map((category) => (
           <option key={category.id} value={category.subject}>{category.subject}</option>
         ))}
