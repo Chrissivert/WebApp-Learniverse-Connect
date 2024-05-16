88db48e8d6b4c8e0c5f14e4d53d25e4cfdcf4ae6
chris <chrisssy@stud.ntnu.no>
2024-05-16 11:45:17 +0200

    rounded filterbuttons

--- frontend/src/components/autocompleteBox/AutoCompleteBox.jsx
+++ frontend/src/components/autocompleteBox/AutoCompleteBox.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './autoCompleteBox.css';
 import { filterLogic } from '../../pages/courses/FilterLogic';
 
 function AutocompleteBox({ courses, filters }) {
