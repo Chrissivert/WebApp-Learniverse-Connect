5b6bc2f69e0e4904afa361bcaf9e71153c10c3e6
Øystein Grande <oystgran@stud.ntnu.no>
2024-05-16 12:25:01 +0200

    Trying to fix CORS Error. Removed Corsconfig file and changed some of the controllers to be marked with @CrossOrigin without URL.

--- backend/src/main/java/no/ntnu/backend/BackendApplication.java
+++ backend/src/main/java/no/ntnu/backend/BackendApplication.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication(
 		exclude = {SecurityAutoConfiguration.class }
 )
-@CrossOrigin("http://localhost:5173")
 public class BackendApplication {
 
 	/**
