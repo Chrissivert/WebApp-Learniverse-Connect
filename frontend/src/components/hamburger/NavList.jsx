4b25589c5438587afeeb5f63c6b5fbbae7532c72
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-16 14:51:14 +0200

    created component NavList to contain all links

---
+++ frontend/src/components/hamburger/NavList.jsx
@@ -0,0 +1,16 @@
+import Button from "../button/Button";
+
+export default function NavList() {
+  return (
+    <ul>
+      <li><Button text='Home' src='/' /></li>
+      <li><Button text='About' src='/about' /></li>
+      <li><Button text='Courses' src='/courses' /></li>
+      <li><Button text='Test [404]' src='*' /></li>
+      <li><Button text='Profile' src='/profile' /></li>
+      <li><Button text='Login' src='/login' /></li>
+      <li><Button text='Register' src='/register' /></li>
+      <li><Button text='Admin' src='/admin' /></li>
+    </ul>
+  );
+}
