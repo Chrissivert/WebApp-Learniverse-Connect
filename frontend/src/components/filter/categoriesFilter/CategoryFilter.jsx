5e463ab64e6a2dc067b0c9f70ff160b9c4ec3a52
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-17 18:15:29 +0200

    Commented out some code, gave an alternative for it. A little more user friendly

--- frontend/src/components/filter/categoriesFilter/CategoryFilter.jsx
+++ frontend/src/components/filter/categoriesFilter/CategoryFilter.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { getCategoriesFromServer } from '../../../services/category-service';
 
 export default function CategoryFilter({ onCategoryChange }) {
-  const [selectedCategory, setSelectedCategory] = useState('');
+  // const [selectedCategory, setSelectedCategory] = useState('');
   const [categories, setCategories] = useState([]);
 
   const handleCategoryChange = (event) => {
     const categoryName = event.target.value;
-    setSelectedCategory(categoryName);
+    // setSelectedCategory(categoryName);
     onCategoryChange(categoryName);
   };
 
@@ -29,11 +29,14 @@ export default function CategoryFilter({ onCategoryChange }) {
   };
 
   return (
-    <div>
-      <label htmlFor="categorySelect">Category:</label>
-      <select id="categorySelect" className="category-select" onChange={handleCategoryChange} value={selectedCategory}>
-        {!selectedCategory && <option value="">-- Select a category --</option>}
-        {selectedCategory && <option value="" style={{ fontWeight: 'bold' }}>-- REMOVE CATEGORY --</option>}
+    <div className='category-container'>
+      {/* <label htmlFor="categorySelect" className='category-label'>Category:</label> */}
+      <label htmlFor="categorySelect" className='category-label'>Select a category:</label>
+      {/* <select id="categorySelect" className="category-select" onChange={handleCategoryChange} value={selectedCategory}> */}
+      <select id="categorySelect" className="category-select" onChange={handleCategoryChange}>
+        {/* {!selectedCategory && <option value="">-- Select a category --</option>}
+        {selectedCategory && <option value="" style={{ fontWeight: 'bold' }}>-- REMOVE CATEGORY --</option>} Thought 'REMOVE CATEGORY' would remove the current category i was on :'( */}
+        {<option value="">All Categories</option>}
         {categories.map((category) => (
           <option key={category.id} value={category.subject}>{category.subject}</option>
         ))}
