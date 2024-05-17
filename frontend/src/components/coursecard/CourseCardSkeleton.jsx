af77c784b40628c1aab414a646690d8fe1f8ba44
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-17 18:59:51 +0200

    Made the coursecard-skeleton look a lot better, by using the actual skeleton in the coursecard.css file

--- frontend/src/components/coursecard/CourseCardSkeleton.jsx
+++ frontend/src/components/coursecard/CourseCardSkeleton.jsx
@@ -3,7 +3,7 @@ import './courseCardSkeleton.css'
 
 const CourseCardSkeleton = () => {
     return (
-        <div className="courseCardSkeleton">
+        <div className="course-card-skeleton">
             <div className="skeleton skeleton-image"></div>
             <div className="skeleton skeleton-text"></div>
             <div className="skeleton skeleton-text short"></div>
