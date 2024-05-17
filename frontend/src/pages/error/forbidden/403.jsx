44e3606d88a37486d83fa76a377c1591cf3c8a01
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-17 18:19:55 +0200

    Renamed a div in 403.jsx

--- frontend/src/pages/error/forbidden/403.jsx
+++ frontend/src/pages/error/forbidden/403.jsx
@@ -3,7 +3,7 @@ import Button from "../../../components/button/Button";
 
 export default function Unauthorized() {
   return (
-    <div className="401-container">
+    <div className="403-container">
       <h1>Error 403: Forbidden Access</h1>
       <p>You do not have permission to access this page.</p>
       <Button text={"Return To Homepage"} src={"/"}/>
