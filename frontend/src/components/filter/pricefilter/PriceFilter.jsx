3fefede4b06cf66849d6b1e6b11639f2c9f0b655
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-18 10:34:23 +0200

    Removed possibility to use 'user input', and rounds up to closest 100.

--- frontend/src/components/filter/pricefilter/PriceFilter.jsx
+++ frontend/src/components/filter/pricefilter/PriceFilter.jsx
@@ -1,109 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
-function PriceRangeFilter({ onPriceChange, maxPrice }) {
-  const [useInput, setUseInput] = useState(false);
+export default function PriceRangeFilter({ onPriceChange, maxPrice }) {
   const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(Math.ceil(maxPrice));
-
-  const handleMinChange = (e) => {
-    let value = parseFloat(e.target.value);
-    value = Math.max(-1, Math.min(value, maxValue));
-    setMinValue(value);
-    onPriceChange(value, maxValue);
-  };
-  
-  const handleMaxChange = (e) => {
-    let value = parseFloat(e.target.value);
-    value = Math.min(Math.ceil(maxPrice), Math.max(value, minValue));
-    setMaxValue(value);
-    onPriceChange(minValue, value);
-  };
-
-  const toggleInputMode = () => {
-    setUseInput((prev) => !prev);
-    if (!useInput && !isNaN(minValue) && !isNaN(maxValue)) {
-      onPriceChange(minValue, maxValue);
-    }
-  };
-
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      toggleInputMode();
-    }
-  };
+  const [maxValue, setMaxValue] = useState(Math.ceil(maxPrice/100)*100);
 
   useEffect(() => {
     onPriceChange(minValue, maxValue);
   }, [minValue, maxValue]);
 
   useEffect(() => {
-    setMaxValue(Math.ceil(maxPrice));
+    setMaxValue(Math.ceil(maxPrice/100)*100);
   }, [maxPrice]);
 
   return (
     <div className="price-range-container">
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={useInput}
-            onChange={toggleInputMode}
-            onKeyPress={handleKeyPress}
-          />
-          Use Input
-        </label>
+      <div className='min-price-range'>
+        <label>Min Price: {minValue}</label>
+        <input
+          type="range"
+          min={0}
+          max={Math.ceil(maxPrice/100)*100}
+          step={100}
+          value={minValue}
+          onChange={(e) => {
+            const value = parseFloat(e.target.value);
+            setMinValue(value);
+          }}
+        />
       </div>
-      {useInput ? (
-        <div>
-          <label htmlFor="minPrice">Min Price:</label>
-          <input
-            type="number"
-            id="minPrice"
-            value={minValue}
-            onChange={handleMinChange}
-            placeholder="Min Price"
-          />
-          <label htmlFor="maxPrice">Max Price:</label>
-          <input
-            type="number"
-            id="maxPrice"
-            value={maxValue}
-            onChange={handleMaxChange}
-            placeholder="Max Price"
-          />
-        </div>
-      ) : (
-        <div>
-          <div>
-            <label>Min Price: {minValue}</label>
-            <input
-              type="range"
-              min={0}
-              max={maxPrice}
-              value={minValue}
-              onChange={(e) => {
-                const value = parseFloat(e.target.value);
-                setMinValue(value);
-              }}
-            />
-          </div>
-          <div>
-            <label>Max Price: {maxValue}</label>
-            <input
-              type="range"
-              min={0}
-              max={maxPrice}
-              value={maxValue}
-              onChange={(e) => {
-                const value = parseFloat(e.target.value);
-                setMaxValue(value);
-              }}
-            />
-          </div>
-        </div>
-      )}
+      <div className='max-price-range'>
+        <label>Max Price: {maxValue}</label>
+        <input
+          type="range"
+          min={0}
+          step={100}
+          max={Math.ceil(maxPrice/100)*100}
+          value={maxValue}
+          onChange={(e) => {
+            const value = parseFloat(e.target.value);
+            setMaxValue(value);
+          }}
+        />
+      </div>
     </div>
   );
 }
-
-export default PriceRangeFilter;
