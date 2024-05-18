310c8115d81ca4b20e500cfe4f0322c4a2e0bb25
Øystein Grande <oystgran@stud.ntnu.no>
2024-05-18 13:31:34 +0200

    User page returns 401 Unauthorized if user is not logged in

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,45 +1,73 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import "./Profile.css";
 //import { PostImage } from "../../components/crudTest/post/image/PostImage";
 //import '../../index.css';
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { getUserByEmail } from "../../services/user-request";
+import { AuthContext } from "../admin/AuthProvider";
+import Unauthorized from "../error/unauthorized/401";
+//import { logout } from "../admin/AuthProvider";
+import { getFavoriteCoursesFromAUser } from "../../services/favorite-course";
+import { AuthProvider } from "../admin/AuthProvider";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   //const [imgId, setImgId] = useState(null);
+  const auth = useContext(AuthContext);
+  const [isUser, setIsUser] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [startDate, setStartDate] = useState("");
+
+  useEffect(() => {
+    console.log(auth.user); // Check if user have been loaded
+    // Check if the user has the user role
+    if (auth.user && auth.user.roles && Array.isArray(auth.user.roles)) {
+      console.log("User roles:", auth.user.roles);
+      const isUser = auth.user.roles.some(
+        (role) => role.authority === "ROLE_USER"
+      );
+      console.log("Is user:", isUser);
+      setIsUser(isUser);
+    }
+    setLoading(false);
+  }, [auth.user]);
+
   useEffect(function () {
     async function getUser() {
       try {
-        const user = JSON.parse(localStorage.getItem("user"));
-        console.log("user" + user.sub);
-        const convertedEmail = user.sub.replace("@", "%40");
-        //console.log("this is my user" + user.sub);
-        console.log("convertedEmail: " + convertedEmail);
-        const res = await getUserByEmail(convertedEmail);
-        const currentUser = res.data;
-        setUserName(currentUser.username);
-        setEmail(currentUser.email);
-        setStartDate(currentUser.startDate);
-        console.log("username: " + userName);
-        console.log("current user: " + currentUser);
-        console.log("email: " + email);
-        console.log("startDate: " + startDate);
-        console.log(res);
-        //const res = await axios.get(`http://localhost:8080/user/${id}`);
-        //setImgId(res.data.imgId);
-        /* const userToken = localStorage.getItem("token");
+        const user = auth.user;
+        console.log("user: " + user);
+        console.log("auth.user: " + user);
+        if (auth.user != null) {
+          console.log("user" + user.sub);
+          const convertedEmail = user.sub.replace("@", "%40");
+          //console.log("this is my user" + user.sub);
+          console.log("convertedEmail: " + convertedEmail);
+          const res = await getUserByEmail(convertedEmail);
+          const currentUser = res.data;
+          setUserName(currentUser.username);
+          setEmail(currentUser.email);
+          setStartDate(currentUser.startDate);
+          console.log("username: " + userName);
+          console.log("current user: " + currentUser);
+          console.log("email: " + email);
+          console.log("startDate: " + startDate);
+          console.log(res);
+          //const res = await axios.get(`http://localhost:8080/user/${id}`);
+          //setImgId(res.data.imgId);
+          /* const userToken = localStorage.getItem("token");
           console.log(userToken); */
 
-        /* if (data.Response === "False") {
+          /* if (data.Response === "False") {
             throw new Error(data.Error);
           }
 
           console.log(data); */
+        }
       } catch (error) {
         console.error("Error fetching user:", error);
       }
@@ -47,6 +75,21 @@ export default function Profile() {
 
     getUser();
   });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // If user is not admin, show unauthorized page
+  if (!isUser) {
+    return (
+      // <div>
+      //     <h1>Unauthorized Access</h1>
+      //     <p>You do not have permission to access this page.</p>
+      // </div>
+      <Unauthorized />
+    );
+  }
   return (
     <div className="profilepage">
       <div className="card">
@@ -61,6 +104,11 @@ export default function Profile() {
       <CourseSelection />
     </div>
   );
+}
+
+function signoff() {
+  localStorage.removeItem("user");
+  localStorage.removeItem("token");
 }
 
 function Avatar() {
@@ -80,8 +128,11 @@ function Intro({ userName, email, startDate }) {
       <h2>{email}</h2>
       <p>User since: {startDate}</p>
       <div className="buttons">
-        <button className="button">Follow</button>
-        <button className="button">Message</button>
+        <Link to="/">
+          <button className="button" onClick={signoff}>
+            Sign Off
+          </button>
+        </Link>
       </div>
     </div>
   );
