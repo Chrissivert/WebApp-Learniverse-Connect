9ccd1d1627ff7a86964460c581e29caf83f1de3f
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-19 15:53:12 +0200

    Made it possible to send mail by clicking on the mail address in the Contact Us section of the Footer

--- frontend/src/layouts/footer/Footer.jsx
+++ frontend/src/layouts/footer/Footer.jsx
@@ -8,12 +8,15 @@ export default function Footer() {
           <h3>Contact Us</h3>
           <ul>
             <li>Address: Larsgårdsvegen 2, Ålesund</li>
-            <li><a href='group01webapp@outlook.com'>Email: info@learniverse-connect.com</a></li>
+            <li><a href='mailto:group01webapp@outlook.com'>Email: info@learniverse-connect.com</a></li>
             <li>Phone: (+47) 735 95 000</li>
           </ul>
         </div>
         <div className='disclaimer'>
-          <p>This website is a result of a university group project, performed in the course <a target="_blank" href='https://www.ntnu.edu/studies/courses/IDATA2301#tab=omEmnet'>IDATA2301 Web technologies</a>, at <a target="_blank" href='https://www.ntnu.edu/'>NTNU</a>. All the information provided here is a result of imagination. Any resemblance with real companies or products is a coincidence.</p>
+          <p>This website is a result of a university group project, performed in the course <a target="_blank" 
+            href='https://www.ntnu.edu/studies/courses/IDATA2301#tab=omEmnet'>IDATA2301 Web technologies</a>, at 
+            <a target="_blank" href='https://www.ntnu.edu/'>NTNU</a>. All the information provided here is a result 
+            of imagination. Any resemblance with real companies or products is a coincidence.</p>
         </div>
         <p className='copyright'>Learniverse Connect © 2024</p>
       </div>
