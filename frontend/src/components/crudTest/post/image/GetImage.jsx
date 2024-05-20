a6191e74c8b9399d763eacc54d7abfffd3fa2af4
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-20 17:49:41 +0200

    imported missing imports

--- frontend/src/components/crudTest/post/image/GetImage.jsx
+++ frontend/src/components/crudTest/post/image/GetImage.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react"
+import { React, useEffect, useState } from "react";
 import { generateImageUrl, getImageDataFromServer } from "../../../../services/image-service";
 
 /**
