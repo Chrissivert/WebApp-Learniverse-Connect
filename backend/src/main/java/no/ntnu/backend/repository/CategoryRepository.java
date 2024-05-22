44ce7457ef5c1b19d3f5f8da02a6023d8a951c55
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 20:22:16 +0200

    Added javadoc to CategoryRepository.java

--- backend/src/main/java/no/ntnu/backend/repository/CategoryRepository.java
+++ backend/src/main/java/no/ntnu/backend/repository/CategoryRepository.java
@@ -6,11 +6,12 @@ import org.springframework.stereotype.Repository;
 import no.ntnu.backend.model.Category;
 
 /**
+ * Repository interface for accessing and managing Category entities in the database.
+ * This interface extends JpaRepository, providing CRUD functionality for Category entities.
  * 
- *
- * @author 
- * @version 29.03.2024
+ * @version 22.05.2024
+ * @author Group 01
  */
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer>{
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
