3a14918ea9360f07725cef7146f228590625e8d9
chris <chrisssy@stud.ntnu.no>
2024-05-22 17:15:47 +0200

    fixed bug where currency wasnt updated when changing currency in profile

--- frontend/src/components/currencySelector/CurrencyContext.jsx
+++ frontend/src/components/currencySelector/CurrencyContext.jsx
@@ -22,3 +22,35 @@ export const CurrencyProvider = ({ children }) => {
     </CurrencyContext.Provider>
   );
 };
+
+
+
+
+
+// import React from 'react';
+// import { useCurrencyContext } from './CurrencyContext.jsx';
+
+// function CurrencySelector({ currencies }) {
+//   const { targetCurrency, handleCurrencyChange } = useCurrencyContext();
+
+//   return (
+//     <div>
+//       <label htmlFor="currencySelect">Select Preferred Currency:</label>
+//       <select
+//         id="currencySelect"
+//         value={targetCurrency}
+//         onChange={(e) => handleCurrencyChange(e.target.value)}
+//         aria-label="Select Preferred Currency"
+//         aria-labelledby="currencySelect"
+//       >
+//         {currencies.map((currency) => (
+//           <option key={currency} value={currency}>
+//             {currency}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// }
+
+// export default CurrencySelector;
