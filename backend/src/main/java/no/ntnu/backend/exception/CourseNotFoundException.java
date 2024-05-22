c391b91e74b56b6a49c96a3d36f078a6baac4e22
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 21:51:05 +0200

    Added javadoc to CourseNotFoundException.java

--- backend/src/main/java/no/ntnu/backend/exception/CourseNotFoundException.java
+++ backend/src/main/java/no/ntnu/backend/exception/CourseNotFoundException.java
@@ -1,8 +1,19 @@
 package no.ntnu.backend.exception;
 
-public class CourseNotFoundException extends RuntimeException{
-    public CourseNotFoundException(Integer id){
-        super("Could not found the user with id "+ id);
-    }
+/**
+ * Exception thrown when a course is not found.
+ * This exception is used to indicate that a requested course does not exist.
+ * 
+ * @version 23.05.2024
+ */
+public class CourseNotFoundException extends RuntimeException {
 
+  /**
+   * Constructs a new CourseNotFoundException with the specified detail message.
+   *
+   * @param id The ID of the course that could not be found.
+   */
+  public CourseNotFoundException(Integer id) {
+    super("Could not find the course with ID " + id);
+  }
 }
