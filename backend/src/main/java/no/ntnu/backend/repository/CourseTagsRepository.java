95b3b5ff13b418f2a0e0b991e31a28fe9549e7d3
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 20:24:15 +0200

    Added javadoc to CourseTagsRepository.java

--- backend/src/main/java/no/ntnu/backend/repository/CourseTagsRepository.java
+++ backend/src/main/java/no/ntnu/backend/repository/CourseTagsRepository.java
@@ -2,15 +2,25 @@ package no.ntnu.backend.repository;
 
 import java.util.List;
 
-import no.ntnu.backend.model.Course;
 import no.ntnu.backend.model.CourseTags;
-import no.ntnu.backend.model.Tags;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+/**
+ * Repository interface for accessing and managing CourseTags entities in the database.
+ * This interface extends JpaRepository, providing CRUD functionality for CourseTags entities.
+ * 
+ * @version 22.05.2024
+ * @author Group 01
+ */
 @Repository
 public interface CourseTagsRepository extends JpaRepository<CourseTags, Integer> {
-    List<CourseTags> findAll();    
+
+    /**
+     * Retrieves all course tags.
+     * 
+     * @return a list of all course tags
+     */
+    List<CourseTags> findAll();
 }
