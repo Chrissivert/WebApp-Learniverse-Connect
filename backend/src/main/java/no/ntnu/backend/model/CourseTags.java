9ba4b3dfb35b9c3fd960d203b42f993ece136196
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 19:50:04 +0200

    Added javadoc and cleaned up code in CourseTags.java

--- backend/src/main/java/no/ntnu/backend/model/CourseTags.java
+++ backend/src/main/java/no/ntnu/backend/model/CourseTags.java
@@ -1,109 +1,199 @@
 package no.ntnu.backend.model;
 
-import java.io.Serializable;
-import java.util.Objects;
-
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import java.io.Serializable;
+import java.util.Objects;
 
+/**
+ * Represents the association between a Course and its Tags with a composite
+ * key.
+ * This class is mapped to a database table using JPA annotations.
+ *
+ * @version 22.05.2024
+ * @author Group 01
+ */
 @Entity
 public class CourseTags {
-    @EmbeddedId
-    private CourseTagsId id;
+  @EmbeddedId
+  private CourseTagsId id;
 
-    public CourseTags() {
-    }
+  /**
+   * Default constructor.
+   */
+  public CourseTags() {
+  }
 
-    public CourseTags(int courseId, int tagId) {
-        this.id = new CourseTagsId(courseId, tagId);
-    }
+  /**
+   * Constructor with course ID and tag ID.
+   *
+   * @param courseId the ID of the course
+   * @param tagId    the ID of the tag
+   */
+  public CourseTags(int courseId, int tagId) {
+    this.id = new CourseTagsId(courseId, tagId);
+  }
 
-    public CourseTagsId getId() {
-        return id;
-    }
+  /**
+   * Gets the composite key of CourseTags.
+   *
+   * @return the composite key
+   */
+  public CourseTagsId getId() {
+    return this.id;
+  }
 
-    public void setId(CourseTagsId id) {
-        this.id = id;
-    }
+  /**
+   * Sets the composite key of CourseTags.
+   *
+   * @param id the composite key to set
+   */
+  public void setId(CourseTagsId id) {
+    this.id = id;
+  }
 
-    public int getCourseId() {
-        return id.getCourse_id();
-    }
+  /**
+   * Gets the ID of the course.
+   *
+   * @return the course ID
+   */
+  public int getCourseId() {
+    return this.id.getCourseId();
+  }
 
-    public void setCourseId(int courseId) {
-        id.setCourse_id(courseId);
-    }
+  /**
+   * Sets the ID of the course.
+   *
+   * @param courseId the course ID to set
+   */
+  public void setCourseId(int courseId) {
+    this.id.setCourseId(courseId);
+  }
 
-    public int getTagId() {
-        return id.getTag_id();
-    }
+  /**
+   * Gets the ID of the tag.
+   *
+   * @return the tag ID
+   */
+  public int getTagId() {
+    return this.id.getTagId();
+  }
 
-    public void setTagId(int tagId) {
-        id.setTag_id(tagId);
-    }
+  /**
+   * Sets the ID of the tag.
+   *
+   * @param tagId the tag ID to set
+   */
+  public void setTagId(int tagId) {
+    this.id.setTagId(tagId);
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        CourseTags that = (CourseTags) obj;
-        return Objects.equals(id, that.id);
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
     }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    CourseTags that = (CourseTags) obj;
+    return Objects.equals(this.id, that.id);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id);
+  }
 
-    @Override
-    public String toString() {
-        return "CourseTags[" +
-                "courseId=" + id.getCourse_id() + ", " +
-                "tagId=" + id.getTag_id() + "]";
-    }
+  @Override
+  public String toString() {
+    return "CourseTags["
+        + "courseId=" + id.getCourseId() + ", "
+        + "tagId=" + id.getTagId() + "]";
+  }
 }
 
+/**
+ * Composite key class for CourseTags, combining course ID and tag ID.
+ * This class is used to represent the composite key in the CourseTags entity.
+ *
+ * @version 22.05.2024
+ * @author Group 01
+ */
 @Embeddable
 class CourseTagsId implements Serializable {
-    private int course_id;
-    private int tag_id;
+  private int courseId;
+  private int tagId;
 
-    public CourseTagsId() {
-    }
+  /**
+   * Default constructor.
+   */
+  public CourseTagsId() {
+  }
 
-    public CourseTagsId(int courseId, int tagId) {
-        this.course_id = courseId;
-        this.tag_id = tagId;
-    }
+  /**
+   * Constructor with course ID and tag ID.
+   *
+   * @param courseId the ID of the course
+   * @param tagId    the ID of the tag
+   */
+  public CourseTagsId(int courseId, int tagId) {
+    this.courseId = courseId;
+    this.tagId = tagId;
+  }
 
-    public int getCourse_id() {
-        return course_id;
-    }
+  /**
+   * Gets the course ID.
+   *
+   * @return the course ID
+   */
+  public int getCourseId() {
+    return this.courseId;
+  }
 
-    public void setCourse_id(int courseId) {
-        this.course_id = courseId;
-    }
+  /**
+   * Sets the course ID.
+   *
+   * @param courseId the course ID to set
+   */
+  public void setCourseId(int courseId) {
+    this.courseId = courseId;
+  }
 
-    public int getTag_id() {
-        return tag_id;
-    }
+  /**
+   * Gets the tag ID.
+   *
+   * @return the tag ID
+   */
+  public int getTagId() {
+    return this.tagId;
+  }
 
-    public void setTag_id(int tagId) {
-        this.tag_id = tagId;
-    }
+  /**
+   * Sets the tag ID.
+   *
+   * @param tagId the tag ID to set
+   */
+  public void setTagId(int tagId) {
+    this.tagId = tagId;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CourseTagsId that = (CourseTagsId) o;
-        return course_id == that.course_id &&
-               tag_id == that.tag_id;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    CourseTagsId that = (CourseTagsId) o;
+    return courseId == that.courseId
+        && tagId == that.tagId;
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(course_id, tag_id);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.courseId, this.tagId);
+  }
 }
