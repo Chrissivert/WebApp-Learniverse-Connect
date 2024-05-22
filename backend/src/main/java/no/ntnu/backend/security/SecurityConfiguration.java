eca38fb5b54c1aec161e5e87a5b229512e74eff1
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 22:01:40 +0200

    Added javadoc to SecurityConfig.Auth.java

--- backend/src/main/java/no/ntnu/backend/security/SecurityConfiguration.java
+++ backend/src/main/java/no/ntnu/backend/security/SecurityConfiguration.java
@@ -22,75 +22,99 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * Configuration class for security settings.
+ */
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private final UserDetailsService userDetailsService;
-    private final JwtRequestFilter jwtRequestFilter;
+  private final UserDetailsService userDetailsService;
+  private final JwtRequestFilter jwtRequestFilter;
 
-    public SecurityConfiguration(UserDetailsService userDetailsService,
-                                 JwtRequestFilter jwtRequestFilter) {
-        this.userDetailsService = userDetailsService;
-        this.jwtRequestFilter = jwtRequestFilter;
-    }
+  /**
+   * Constructor for SecurityConfiguration.
+   *
+   * @param userDetailsService UserDetailsService implementation
+   * @param jwtRequestFilter   JwtRequestFilter instance
+   */
+  public SecurityConfiguration(UserDetailsService userDetailsService,
+      JwtRequestFilter jwtRequestFilter) {
+    this.userDetailsService = userDetailsService;
+    this.jwtRequestFilter = jwtRequestFilter;
+  }
 
+  /**
+   * This method will be called automatically by the framework to find the
+   * authentication to use.
+   * Here we tell that we want to load users from a database
+   *
+   * @param auth Authentication builder
+   * @throws Exception When user service is not found
+   */
 
-    /**
-     * This method will be called automatically by the framework to find the authentication to use.
-     * Here we tell that we want to load users from a database
-     *
-     * @param auth Authentication builder
-     * @throws Exception When user service is not found
-     */
+  // private static final Logger logger =
+  // LoggerFactory.getLogger(CorsConfig.class);
+  @Autowired
+  protected void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService);
+  }
 
-    //private static final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
-    @Autowired
-    protected void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-    }
+  /**
+   * This method will be called automatically by the framework to find the
+   * authentication to use.
+   *
+   * @param http HttpSecurity setting builder
+   * @throws Exception When security configuration fails
+   */
+  @Bean
+  public SecurityFilterChain configureAuthorizationFilterChain(HttpSecurity http) throws Exception {
+    // logger.info("Using SecurityFilterChain...");
+    http
+        // Disable CSRF and CORS checks. Without this it will be hard to make automated
+        // tests.
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        // Allow HTTP OPTIONS requests - CORS pre-flight requests
+        .authorizeHttpRequests((auth) -> auth.requestMatchers(HttpMethod.OPTIONS).permitAll())
+        // Admin URL is accessible only for Admin role
+        .authorizeHttpRequests((auth) -> auth.requestMatchers("api/admin/**").hasRole("ADMIN"))
+        // Users URL is accessible only for User role
+        // .authorizeHttpRequests((auth) ->
+        // auth.requestMatchers("api/users").hasAnyRole("USER", "ADMIN"))
+        .authorizeHttpRequests((auth) -> auth.requestMatchers("api/users/**").hasAnyRole("USER", "ADMIN"))
+        // Other URLs are accessible by everyone.
 
-    /**
-     * This method will be called automatically by the framework to find the authentication to use.
-     *
-     * @param http HttpSecurity setting builder
-     * @throws Exception When security configuration fails
-     */
-    @Bean
-    public SecurityFilterChain configureAuthorizationFilterChain(HttpSecurity http) throws Exception {
-        //logger.info("Using SecurityFilterChain...");
-        http
-                // Disable CSRF and CORS checks. Without this it will be hard to make automated tests.
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                // Allow HTTP OPTIONS requests - CORS pre-flight requests
-                .authorizeHttpRequests((auth) -> auth.requestMatchers(HttpMethod.OPTIONS).permitAll())
-                // Admin URL is accessible only for Admin role
-                .authorizeHttpRequests((auth) -> auth.requestMatchers("api/admin/**").hasRole("ADMIN"))
-                // Users URL is accessible only for User role
-                //.authorizeHttpRequests((auth) -> auth.requestMatchers("api/users").hasAnyRole("USER", "ADMIN"))
-                .authorizeHttpRequests((auth) -> auth.requestMatchers("api/users/**").hasAnyRole("USER", "ADMIN"))
-                // Other URLs are accessible by everyone.
-                
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
+        .authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll())
 
-                // Enable stateless session policy
-                .sessionManagement((session) ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                // Enable our JWT authentication filter
-                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }
+        // Enable stateless session policy
+        .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // Enable our JWT authentication filter
+        .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    return http.build();
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+  /**
+   * Configure the password encoder.
+   *
+   * @return PasswordEncoder instance
+   */
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
-            throws Exception {
-        return config.getAuthenticationManager();
-    }
+  /**
+   * Configure the authentication manager.
+   *
+   * @param config AuthenticationConfiguration instance
+   * @return AuthenticationManager instance
+   * @throws Exception When configuration fails
+   */
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
+  }
 }
