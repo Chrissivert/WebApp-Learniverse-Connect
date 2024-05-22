de384cca16de027252d2bed167b15caf1c34b127
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 20:06:24 +0200

    Added javadoc and cleaned up code in Role.java

--- backend/src/main/java/no/ntnu/backend/model/Role.java
+++ backend/src/main/java/no/ntnu/backend/model/Role.java
@@ -1,13 +1,19 @@
 package no.ntnu.backend.model;
 
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Set;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
 
-import jakarta.persistence.*;
-
+/**
+ * Represents a role entity with a title.
+ * This class is mapped to a database table using JPA annotations.
+ *
+ * @version 22.05.2024
+ * @author Group 01
+ */
 @Entity
 public class Role {
   @Id
@@ -15,53 +21,89 @@ public class Role {
   private int id;
   private String title;
 
+  /**
+   * Default constructor.
+   */
   public Role() {
   }
 
-  public Role(String title){this.title = title;}
+  /**
+   * Constructor with title.
+   *
+   * @param title the title of the role
+   */
+  public Role(String title) {
+    this.title = title;
+  }
 
+  /**
+   * Gets the ID of the role.
+   *
+   * @return the ID of the role
+   */
   public int getId() {
     return this.id;
   }
 
+  /**
+   * Sets the ID of the role.
+   *
+   * @param id the new ID of the role
+   */
   public void setId(int id) {
     this.id = id;
   }
 
-  //public void setUsers(Set<User> users){this.users = users;}
-
+  /**
+   * Gets the title of the role.
+   *
+   * @return the title of the role
+   */
   public String getTitle() {
     return this.title;
   }
 
+  /**
+   * Sets the title of the role.
+   *
+   * @param title the new title of the role
+   */
   public void setTitle(String title) {
     this.title = title;
   }
 
+  /**
+   * Checks if the role is valid based on its attributes.
+   *
+   * @return true if the role is valid, false otherwise
+   */
   @JsonIgnore
   public boolean isValid() {
-    return //this.id > 0 &&
+    return // this.id > 0 &&
       !this.title.isBlank() && this.title != null;
   }
 
   @Override
   public boolean equals(Object obj) {
-    if (obj == this)
+    if (obj == this) {
       return true;
-    if (obj == null || obj.getClass() != this.getClass())
+    }
+    if (obj == null || obj.getClass() != this.getClass()) {
       return false;
+    }
     var that = (Role) obj;
     return this.id == that.id;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(this.id);
+    return Objects.hash(this.id, this.title);
   }
 
   @Override
   public String toString() {
-    return "Provider[" +
-        "id=" + this.id + ']';
+    return "Role[" 
+      + "id=" + this.id 
+      + "title=" + this.title + ']';
   }
 }
