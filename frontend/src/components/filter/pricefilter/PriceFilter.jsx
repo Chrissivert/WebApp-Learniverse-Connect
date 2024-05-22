4ce2ac4cfb410a27539a7b3a879777354708ad15
chris <chrisssy@stud.ntnu.no>
2024-05-22 19:03:22 +0200

    price range works although incorrect maxprice

--- frontend/src/components/filter/pricefilter/PriceFilter.jsx
+++ frontend/src/components/filter/pricefilter/PriceFilter.jsx
@@ -1,36 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Slider from 'react-slider';
 import './priceFilter.css';
 
 export default function PriceRangeFilter({ onPriceChange, maxPrice }) {
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(Math.ceil(maxPrice / 100) * 100);
-
+  const [sliderValues, setSliderValues] = useState([0, Math.ceil(maxPrice / 100) * 100]);
+  const initialMaxPrice = useRef(maxPrice); // Store the initial maxPrice
 
   useEffect(() => {
-    setMaxValue(Math.ceil(maxPrice / 100) * 100);
-  }, [maxPrice]);
+    onPriceChange(sliderValues[0], sliderValues[1]);
+  }, [sliderValues, onPriceChange]);
 
-  const handleSliderChange = (values) => {
-    setMinValue(values[0]);
-    setMaxValue(values[1]);
+  // Handle slider value changes
+  const handleSliderChange = (newValues) => {
+    setSliderValues(newValues);
   };
 
   return (
     <div className="slider-container">
       <div className="slider-box">
         <h3>Price <span>Range</span></h3>
-        <div className="values">{minValue} - {maxValue}</div>
+        <div className="values">{sliderValues[0]} - {sliderValues[1]}</div>
         <small>
-          Current Range: {minValue} - {maxValue}
+          Current Range: {sliderValues[0]} - {sliderValues[1]}
         </small>
         <Slider
           className="slider"
           onChange={handleSliderChange}
-          value={[minValue, maxValue]}
+          value={sliderValues}
           min={0}
           step={100}
-          max={Math.ceil(maxPrice / 100) * 100}
+          max={Math.ceil(initialMaxPrice.current / 100) * 100} // Use initialMaxPrice.current
         />
       </div>
     </div>
