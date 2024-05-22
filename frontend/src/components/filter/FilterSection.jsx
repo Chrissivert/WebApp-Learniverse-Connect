929da095a144559c9d116b8ee7a5359853342759
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 11:13:34 +0200

    Styled the filter section to be centered with the coursePage

--- frontend/src/components/filter/FilterSection.jsx
+++ frontend/src/components/filter/FilterSection.jsx
@@ -3,17 +3,18 @@ import SearchBar from '../../components/filter/searchBar/SearchBar';
 import PriceRangeFilter from '../../components/filter/pricefilter/PriceFilter';
 import SortByFilter from './sortbyfilter/SortByFilter';
 import CategoryFilter from './categoriesFilter/CategoryFilter';
-import AutocompleteBox from '../../components/autocompleteBox/AutoCompleteBox'; // Import AutocompleteBox component
-import './filterSection.css'; // Import CSS file for FilterSection component
+import AutocompleteBox from '../../components/autocompleteBox/AutoCompleteBox';
+import './filterSection.css';
 
 export default function FilterSection({ onSearchQueryChange, onPriceChange, onSortChange, onCategoryChange, courses, filters, maxPrice }) {
   return (
     <section className='filter-section-container'>
-      <SearchBar setSearchQuery={onSearchQueryChange} />
-      <AutocompleteBox courses={courses} filters={filters} /> {/* Pass both courses and filters to AutocompleteBox */}
+      <div className='search-row'>
+        <SearchBar setSearchQuery={onSearchQueryChange} />
+        <AutocompleteBox courses={courses} filters={filters} />
+      </div>
       <div className="filter-row" role="group" aria-labelledby="filterHeading">
-        <h2 id="filterHeading" className="visually-hidden"></h2>
-        <PriceRangeFilter onPriceChange={onPriceChange} maxPrice={maxPrice} /> {/* Pass maxPrice as a prop */}
+        <PriceRangeFilter onPriceChange={onPriceChange} maxPrice={maxPrice} />
         <SortByFilter onSortChange={onSortChange} />
         <CategoryFilter onCategoryChange={onCategoryChange} />
       </div>
