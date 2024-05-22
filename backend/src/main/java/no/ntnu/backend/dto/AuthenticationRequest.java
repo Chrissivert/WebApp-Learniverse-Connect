f9b18a135bd4bd175b429e5cb277bae2332e3039
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 21:44:46 +0200

    Added javadoc to Auth.Request.java

--- backend/src/main/java/no/ntnu/backend/dto/AuthenticationRequest.java
+++ backend/src/main/java/no/ntnu/backend/dto/AuthenticationRequest.java
@@ -1,37 +1,86 @@
 package no.ntnu.backend.dto;
 
-
+/**
+ * Data Transfer Object (DTO) for handling authentication requests.
+ * Contains the necessary fields for user authentication.
+ * 
+ * @version 23.05.2024
+ */
 public class AuthenticationRequest {
 
   private int id;
   private String email;
   private String password;
 
+  /**
+   * Default constructor for AuthenticationRequest.
+   */
   public AuthenticationRequest() {
   }
 
+  /**
+   * Parameterized constructor for AuthenticationRequest.
+   *
+   * @param id       The ID of the user.
+   * @param email    The email of the user.
+   * @param password The password of the user.
+   */
   public AuthenticationRequest(int id, String email, String password) {
     this.id = id;
     this.email = email;
     this.password = password;
   }
 
-  public int getId(){return this.id;}
+  /**
+   * Gets the ID of the user.
+   *
+   * @return The ID of the user.
+   */
+  public int getId() {
+    return this.id;
+  }
 
-  public void setId(){this.id = id;}
+  /**
+   * Sets the ID of the user.
+   *
+   * @param id The ID of the user.
+   */
+  public void setId(int id) {
+    this.id = id;
+  }
 
+  /**
+   * Gets the email of the user.
+   *
+   * @return The email of the user.
+   */
   public String getEmail() {
     return this.email;
   }
 
+  /**
+   * Sets the email of the user.
+   *
+   * @param email The email of the user.
+   */
   public void setEmail(String email) {
     this.email = email;
   }
 
+  /**
+   * Gets the password of the user.
+   *
+   * @return The password of the user.
+   */
   public String getPassword() {
     return this.password;
   }
 
+  /**
+   * Sets the password of the user.
+   *
+   * @param password The password of the user.
+   */
   public void setPassword(String password) {
     this.password = password;
   }
