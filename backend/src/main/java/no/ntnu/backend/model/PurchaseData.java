0606da84b2f57d9b80a75ce0a22608542de86a86
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 20:04:09 +0200

    Added javadoc and cleaned up code in PurchaseData.java

--- backend/src/main/java/no/ntnu/backend/model/PurchaseData.java
+++ backend/src/main/java/no/ntnu/backend/model/PurchaseData.java
@@ -1,32 +1,68 @@
 package no.ntnu.backend.model;
 
+/**
+ * Represents data related to a purchase, including email details such as
+ * recipient, subject, and text.
+ *
+ * @version 22.05.2024
+ * @author Group 01
+ */
 public class PurchaseData {
-    private String toEmail;
-    private String subject;
-    private String text;
+  private String toEmail;
+  private String subject;
+  private String text;
 
-    // Getters and setters
-    public String getToEmail() {
-        return toEmail;
-    }
+  /**
+   * Gets the email address of the recipient.
+   *
+   * @return the email address of the recipient
+   */
+  public String getToEmail() {
+    return this.toEmail;
+  }
 
-    public void setToEmail(String toEmail) {
-        this.toEmail = toEmail;
-    }
+  /**
+   * Sets the email address of the recipient.
+   *
+   * @param toEmail the email address of the recipient
+   */
+  public void setToEmail(String toEmail) {
+    this.toEmail = toEmail;
+  }
 
-    public String getSubject() {
-        return subject;
-    }
+  /**
+   * Gets the subject of the email.
+   *
+   * @return the subject of the email
+   */
+  public String getSubject() {
+    return this.subject;
+  }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
+  /**
+   * Sets the subject of the email.
+   *
+   * @param subject the subject of the email
+   */
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
 
-    public String getText() {
-        return text;
-    }
+  /**
+   * Gets the text content of the email.
+   *
+   * @return the text content of the email
+   */
+  public String getText() {
+    return this.text;
+  }
 
-    public void setText(String text) {
-        this.text = text;
-    }
+  /**
+   * Sets the text content of the email.
+   *
+   * @param text the text content of the email
+   */
+  public void setText(String text) {
+    this.text = text;
+  }
 }
