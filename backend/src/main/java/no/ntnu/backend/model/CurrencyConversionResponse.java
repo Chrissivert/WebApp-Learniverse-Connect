13b41b456795c3500b151a4e8f872db2d68f5f90
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 19:50:41 +0200

    Added javadoc and cleaned up code in CurrencyConversionResponse.java

--- backend/src/main/java/no/ntnu/backend/model/CurrencyConversionResponse.java
+++ backend/src/main/java/no/ntnu/backend/model/CurrencyConversionResponse.java
@@ -2,14 +2,35 @@ package no.ntnu.backend.model;
 
 import java.util.Map;
 
+/**
+ * Represents a response object for currency conversion, containing a map of
+ * currency codes to conversion rates.
+ * This class is used to deserialize JSON responses from currency conversion
+ * APIs.
+ *
+ * @version 22.05.2024
+ * @author Group 01
+ */
 public class CurrencyConversionResponse {
-    private Map<String, Double> data;
+  private Map<String, Double> data;
 
-    public Map<String, Double> getData() {
-        return data;
-    }
+  /**
+   * Gets the map containing currency codes as keys and conversion rates as
+   * values.
+   *
+   * @return the map of currency codes to conversion rates
+   */
+  public Map<String, Double> getData() {
+    return this.data;
+  }
 
-    public void setData(Map<String, Double> rates) {
-        this.data = rates;
-    }
+  /**
+   * Sets the map containing currency codes as keys and conversion rates as
+   * values.
+   *
+   * @param rates the map of currency codes to conversion rates to set
+   */
+  public void setData(Map<String, Double> rates) {
+    this.data = rates;
+  }
 }
