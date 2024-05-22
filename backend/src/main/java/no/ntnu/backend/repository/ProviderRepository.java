9d48a5bb87af2973eb4ddcbf9510a79dcc91eb0d
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2024-05-22 20:26:59 +0200

    Added javadoc to ProviderRepository.java

--- backend/src/main/java/no/ntnu/backend/repository/ProviderRepository.java
+++ backend/src/main/java/no/ntnu/backend/repository/ProviderRepository.java
@@ -5,7 +5,15 @@ import no.ntnu.backend.model.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+/**
+ * Repository interface for accessing and managing Provider entities in the
+ * database.
+ * This interface extends JpaRepository, providing CRUD functionality for
+ * Provider entities.
+ * 
+ * @version 22.05.2024
+ * @author Group 01
+ */
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Integer> {
 }
